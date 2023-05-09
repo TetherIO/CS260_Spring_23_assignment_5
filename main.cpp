2bee3173285c8cc8ed4814802ba2ba4eb28b2d3f
TetherIO <96389721+TetherIO@users.noreply.github.com>
2023-05-08 21:32:05 -0700

    Syntax errors fixed and added comments
    Fixed some issues with brackets and formatting. Also included comments to better explain search function.

--- main.cpp
+++ main.cpp
@@ -23,8 +23,8 @@ public:
     list() {
         head = nullptr;
     }
-    
-    
+
+
     // Insert data into new node in list of ascending order
     void insert(int data) {
         // Create a new node with the given data
@@ -41,32 +41,30 @@ public:
 
         // inserting new node
         node* current = head;
-        
+
         while (current->next != nullptr && data > current->next->data) { // checks if next node exists and data greater than data of next node
             current = current->next;  // will point to node after which new node should be inserted
         }
 
-                new_node->next = current->next; // sets next pointer of new node to node that comes after current node 
-                current->next = new_node; // current now can point to new node, allows for insert of new node between current and current->next
-            }
-
-        }
+        new_node->next = current->next; // sets next pointer of new node to node that comes after current node
+        current->next = new_node; // current now can point to new node, allows for insert of new node between current and current->next
+    }
 
 
 // Binary search of linked list
-bool list_search(int searched) {
-    node* current = head; // sets current node pointer
-    while (current != nullptr) {
-        if (current ->data == searched) {
-            return true;
+    bool list_search(int searched) {
+        node* current = head; // sets current node pointer
+        while (current != nullptr) {
+            if (current ->data == searched) {
+                return true; // returns boolean value if found, but does not include the depth of the node (perhaps implementing that later if needed?)
+            }
+            current = current->next; // if not found, continues through list until nullptr reached
         }
-        current = current->next;
+        return false; // returns false if no values in loop matched (that would have caused true to return
     }
-    return false;
-} 
 
 
-// copied from the instructor's demo, updated with my nullptr usage
+    // copied from the instructor's demo, updated with my nullptr usage
     void display() {
         node* current = head;
 
@@ -76,37 +74,37 @@ bool list_search(int searched) {
         }
         cout << endl;
     }
-};
+    };
 
 int main() {
     list my_list;
 
     // insert some nodes
-    my_list.insert(1, 0);
+    my_list.insert(1);
     my_list.display();
 
-    my_list.insert(2, 1);
+    my_list.insert(2);
     my_list.display();
 
-    my_list.insert(3, 1);
+    my_list.insert(3);
     my_list.display();
 
-    my_list.insert(4, 3);
+    my_list.insert(4);
     my_list.display();
 
-    my_list.insert(5, 2);
+    my_list.insert(5);
     my_list.display();
 
     // after end of list
-    my_list.insert(6, 100);
+    my_list.insert(6);
     my_list.display();
 
     // at the head
-    my_list.insert(7, 0);
+    my_list.insert(7);
     my_list.display();
 
     // before the head
-    my_list.insert(8, -10);
+    my_list.insert(8);
     my_list.display();
 
     return 0;
